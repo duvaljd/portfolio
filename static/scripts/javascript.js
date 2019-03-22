3b871aa06f305bd78ad47091d7dddf990551b491
jd <duvaljd@umich.edu>
2019-03-22 19:55:19 -0400

    Reworked semantic markup for HTML sections

--- static/scripts/javascript.js
+++ static/scripts/javascript.js
@@ -61,13 +61,13 @@ function jump(elID) {
 //Sticky navigation
 $(function() {
 
- var brand_Container = $('.sidebar .container');
+ var brand_Container = $('.page-sidebar .container');
  var brand_Height = brand_Container.outerHeight();
  var brand = $('nav h1');
- var header = $('header');
- var header_Height = header.outerHeight();
+ var header_Height = $('header').outerHeight();
  var header_h1 = $('header h1');
  var header_h2 = $('header h2');
+ var winWidth = $('body').outerWidth() * .80;
 
  header_h2.waypoint({
   handler: function(direction) {
@@ -75,7 +75,7 @@ $(function() {
    if (direction == 'down') {
     header_h1.css({'display': 'none'});
     header_h2.css({'display': 'none'});
-    brand.css({'display': 'flex', 'position': 'fixed', 'color': '#fff', 'padding-top': '5vh'});
+    brand.css({'display': 'flex', 'position': 'fixed', 'color': '#fff', 'padding-top': '5vh', 'right': winWidth});
     console.log("Yes I'm triggering");
 
    } else {
@@ -88,9 +88,24 @@ $(function() {
  });
 });
 
-$(".section-anchor").click(function(event){
+$('a.anchor').click(function(event){
  var windowHeight = $(window).height();   
  event.preventDefault();
  $('html,body').animate({
   scrollTop: $(this.hash).offset().top + -(.05 * windowHeight)}, 500);
-});
+ });
+
+
+$(function() {
+ var sections = $('section');
+
+ for (var s = 0; i < sections.length; i++) {
+  new Waypoint({
+   element: sections[s],
+   handler: function() {
+    console.log('we did make it here');
+    $(this).css({'border': '1px solid red'});
+    }
+   })
+  }
+ })
