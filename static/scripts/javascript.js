98fad523f74d67ff66111899b5dcdcb049a52c28
jd <duvaljd@umich.edu>
2019-04-17 20:32:06 -0400

    F I N A L L Y got css scroll snap working in Chrome and Firefox, with links highlighting correctly via Waypoints. Firefox still scrolling anchor links to top...

--- static/scripts/javascript.js
+++ static/scripts/javascript.js
@@ -1,5 +1,5 @@
-//// STICKY NAVIGATION
-function stickynav() {
+//// STICKY NAVIGATION (waypoints.js)
+function updateNav() {
 
  var navigation = $('nav');
  var trigger = $('header h1');
@@ -18,140 +18,79 @@ function stickynav() {
   });
 };
 
-//// ANCHOR LINKS
-function jump() {
- $('a').on('click', function(event) {
-  if (this.hash !== "") {
-   event.preventDefault();
-   var hash = this.hash.substring(1, this.hash.length);
+//// HIGHLIGHT LINKS (waypoints.js)
 
-   var el = document.getElementById(hash);
-   pos = el.style.position;
-   top = el.style.top;
-
-   el.style.position ='relative';
-   el.scrollIntoView({behavior: 'smooth', block: 'start'});
-   el.style.top = top;
-   el.style.position = pos;
-  };
- });
-};
-
-
-//// HIGHLIGHT NAV ON SCROLL
-
-// Helper functions
+/// Helper Function
 function getRelatedNavigation(id) {
  return $('a[href="#' + id +'"]')
 };
 
-// Open & highlight nav section
+/// Main Navigation
+// Add main navigation highlight
 $('section').waypoint(function(direction) {
  var navlink = getRelatedNavigation(this.element.id);
 
  $(navlink).toggleClass('active', direction == 'down');
-}, {
+ }, {
  offset: '25%',
  context: '.page-content'
 });
 
-// Highlight active section links
-$('.content-block').waypoint(function(direction) {
- var navlink = getRelatedNavigation(this.element.id);
-
- $(navlink).toggleClass('active', direction == 'down');
-}, {
- offset: '25%',
- context: '.page-content'
-});
-
-// Remove section highlight
-$('.content-block').waypoint(function(direction) {
+// Remove main navigation highlight
+$('section').waypoint(function(direction) {
  var navlink = getRelatedNavigation(this.element.id);
 
  $(navlink).toggleClass('active', direction == 'up');
-}, {
+ }, {
  offset: function() {
   return -$(this.element).height() + 100;
  },
  context: '.page-content'
 });
 
-// Remove nav highlight and close section
-$('section').waypoint(function(direction) {
+/// Section Navigation
+// Add section navigation highlight
+$('.slide').waypoint(function(direction) {
  var navlink = getRelatedNavigation(this.element.id);
 
- $(navlink).toggleClass('active', direction == 'up');
-}, {
- offset: function() {
-  return -$(this.element).height() + 100;
- },
- context: '.page-content'
+ $(navlink).toggleClass('active', direction == 'right');
+ }, {
+ offset: '25%',
+ context: '.section-content',
+ horizontal: true,
 });
 
-//// SLIDESHOW
-var slideIndex = 1;
-var z = document.getElementsByClassName("slideshow");
+// Remove section navigation highlight
+$('.slide').waypoint(function(direction) {
+ var navlink = getRelatedNavigation(this.element.id);
 
-for (i = 0; i < z.length; i++) {
-  z[i].setAttribute("this-slide", 1);
-  showDivs(z[i].getAttribute("this-slide"), i);
+ $(navlink).toggleClass('active', direction == 'left');
+ }, {
+ offset: function() {
+  return -$(this.element).width() + 100;
+ },
+ context: '.section-content',
+ horizontal: true,
+});
+
+
+fixHeight = function() {
+ var div = $
 }
-
-function plusDivs(n, j) {
-  slideIndex = parseInt(z[j].getAttribute("this-slide")[0]);
-  showDivs(slideIndex += n, j);
-}
-
-function showDivs(n, j) {
-  var i;
-  var z = document.getElementsByClassName("slideshow")[j];
-  var x = z.getElementsByClassName("slide");
-  var subs = z.getElementsByClassName("sub");
-
-  if (n > x.length) {
-    slideIndex = 1
-  }
-
-  if (n < 1) {
-    slideIndex = x.length;
-  }
-
-  z.setAttribute("this-slide", slideIndex);
-
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-
-  for (i = 0; i < subs.length; i++) {
-    subs[i].className = subs[i].className.replace(" active", "");
-  }
-
-  x[slideIndex - 1].style.display = "flex";
-  subs[slideIndex - 1].className += " active";
-}
-
-function currentDiv(n, j) {
- showDivs(slideIndex = n, j);
- };
 
 //// EXECUTE
 
-// When document is ready
+/// When document is ready
 $(document).ready(function() {
- stickynav();
- jump();
- })
+ updateNav();
+ });
 
-
-// When window is resized
+/// When window is resized
 $(window).resize(function() {
- stickynav();
- jump();
- })
+ updateNav();
+ });
 
-// When window is zoomed
+/// When window is zoomed
 $(window).on('zoom', function() {
- stickynav();
- jump();
-})
+ updateNav();
+ });
